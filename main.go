d2f0a062adf60b9ce4c3629a237bd513e29c0ceb
salemzii <salemododa2@gmail.com>
2022-08-03 16:52:15 +0100

    deploying service

--- main.go
+++ main.go
@@ -7,22 +7,12 @@ import (
 
 func main() {
 
-	alert := app.Alert{
-		Body:      "Alert Body with context",
-		Platforms: []app.Platform{{Name: "twitter"}, {Name: "facebook"}},
-		Urls: []app.Url{{Uri: "http://127.0.0.1:8000/webhook1"}, {Uri: "http://127.0.0.1:8000/webhook2"},
-			{Uri: "http://127.0.0.1:8000/webhook3"}},
-	}
-	app.CreateAlerts(alert)
+	router := gin.Default()
 
-	/*
-		router := gin.Default()
+	router.GET("/", welcome)
+	router.POST("/createalert", app.ReceiveAlert)
 
-		router.GET("/", welcome)
-		router.POST("/createalert", app.ReceiveAlert)
-
-		router.Run()
-	*/
+	router.Run()
 
 }
 
